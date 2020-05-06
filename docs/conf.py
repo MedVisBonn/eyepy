8faa2d7b87c081efd875f59ed50af958c0858815
Olivier Morelle <oli4morelle@gmail.com>
2020-05-06 23:29:05 +0200

    Added sphinx extensions for numpy style docstrings (napoleon) and typehints (sphinx_autodoc_typehints)

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ import eyepy
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc',
+              'sphinx_autodoc_typehints', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
