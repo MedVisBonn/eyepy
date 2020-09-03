c2a3ed8b23fc8c23a6a7b159528e065340dc32f2
Olivier Morelle <oli4morelle@gmail.com>
2020-09-03 13:53:05 +0200

    relaxed dependencies for now

--- setup.py
+++ setup.py
@@ -12,11 +12,10 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "imageio==2.6.1",
-    "numpy==1.17.3",
-    "matplotlib==3.1.1",
-    "scikit-image==0.16.2",
-    "untangle==1.1.1"
+    "imageio",
+    "numpy",
+    "matplotlib",
+    "scikit-image"
 ]
 
 setup_requirements = ["pytest-runner"]
