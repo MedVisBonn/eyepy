added drusen metrics and fixed drusen computation

eyepy/core/drusen.py
@@ -9,15 +9,32 @@ def drusen(rpe_height, bm_height, scan_shape, degree=3, iterations=5,
     First estimate the normal RPE by fitting a polynomial to the RPE. Then
     compute drusen as the area between the RPE and the normal RPE
     """
+
+    rpe_height = np.rint(interpolate_layer(rpe_height))
+    bm_height = np.rint(interpolate_layer(bm_height))
     # Estimate normal RPE
     normal_rpe_height = normal_rpe(rpe_height, bm_height, scan_shape, degree,
                                    iterations, outlier_threshold,
                                    poly_fit_type)
     # Create drusen map
-    drusen_map = np.zeros(scan_shape)
+    drusen_map = np.zeros(scan_shape, dtype=bool)
+
     # Exclude normal RPE and RPE from the drusen area.
-    drusen_map[normal_rpe_height + 1:rpe_height] = 1
+    for col in range(drusen_map.shape[1]):
+        drusen_map[(rpe_height + 1).astype(int)[col]: normal_rpe_height.astype(int)[col], col] = 1
+    # drusen_ranges = [np.s_[start, stop] for start, stop in zip(normal_rpe_height.astype(int) + 1, rpe_height.astype(int))]
+    # drusen_map[drusen_ranges, :] = 1
     return drusen_map
+
+
+def interpolate_layer(layer_height, kind="nearest"):
+    nans = np.isnan(layer_height)
+    x = np.arange(layer_height.shape[0])[~nans]
+    y = layer_height[~nans]
+    f = interp1d(x, y, kind=kind, fill_value="extrapolate")
+    interpolated_layer = np.copy(layer_height)
+    interpolated_layer[nans] = f(np.argwhere(nans)[:, 0])
+    return interpolated_layer
 
 
 def normal_rpe(rpe_height, bm_height, scan_shape, degree=3, iterations=5,
@@ -31,11 +48,6 @@ def normal_rpe(rpe_height, bm_height, scan_shape, degree=3, iterations=5,
     """
     h, w = scan_shape
 
-    # interpolate NANs in BM
-    nans = np.isnan(bm_height)
-    f = interp1d(np.arange(w)[~nans], bm_height[~nans], kind="nearest")
-    bm_height[nans] = f(np.argwhere(nans))
-
     # compute shift needed to align the BM to the horizontal center line
     shift = np.empty(w, dtype='int')
     shift.fill(h - (h / 2))
@@ -45,7 +57,7 @@ def normal_rpe(rpe_height, bm_height, scan_shape, degree=3, iterations=5,
     shifted_rpe_height = rpe_height + shift
 
     # These variables change while outliers are removed and polynomial fitting
-    tmpx = range(len(rpe_height))
+    tmpx = range(w)
     tmpy = np.copy(shifted_rpe_height)
 
     it = 0
@@ -62,7 +74,7 @@ def normal_rpe(rpe_height, bm_height, scan_shape, degree=3, iterations=5,
         if outlier_threshold:
             # Compute norm rpe with these values in next iteration
             inlier = norm_rpe - shifted_rpe_height < outlier_threshold
-            tmpx = np.argwhere(inlier)
+            tmpx = np.argwhere(inlier)[:, 0]
             tmpy = shifted_rpe_height[inlier]
         else:
             # TODO: Is this correct

eyepy/core/octbase.py
@@ -55,10 +55,10 @@ class Oct(ABC):
     @property
     def drusen(self):
         if self._drusen is None:
-            self._drusen = np.stack([x.drusen for x in self._bscans], axis=1)
+            self._drusen = np.stack([x.drusen for x in self._bscans], axis=-1)
         return self._drusen
 
-    def plot(self, ax=None, bscan_positions=None, line_kwargs=None):
+    def plot(self, ax=None, bscan_positions=None, line_kwargs={"linewidth": 0.5, "color": "green"}):
         """ Plot Slo with localization of corresponding B-Scans"""
 
         if ax is None:
@@ -80,9 +80,10 @@ class Oct(ABC):
 
         for i in bscan_positions:
             bscan = self[i]
-            ax.plot(np.array([bscan.StartX, bscan.EndX]) / bscan.ScaleXSlo,
-                    np.array([bscan.StartX, bscan.EndX]) / bscan.ScaleXSlo,
-                    **line_kwargs)
+            x = np.array([bscan.StartX, bscan.EndX]) / self.ScaleXSlo
+            y = np.array([bscan.StartY, bscan.EndY]) / self.ScaleYSlo
+
+            ax.plot(x, y, **line_kwargs)
 
     def plot_slo_bscan(self, ax=None, n_bscan=0):
         """ Plot Slo with one selected B-Scan """
@@ -134,7 +135,8 @@ class Bscan:
         return drusen(self.segmentation["RPE"], self.segmentation["BM"],
                       self.scan.shape)
 
-    def plot(self, ax=None, layers=None, layers_kwargs=None, layers_color=None, layers_only=False):
+    def plot(self, ax=None, layers=None, drusen=False, layers_kwargs=None, layers_color=None,
+             annotation_only=False):
         """ Plot B-Scan with segmented Layers """
         if ax is None:
             fig, ax = plt.subplots(1, 1)
@@ -156,8 +158,12 @@ class Bscan:
         else:
             layers_color = {**config.layers_color, **layers_color}
 
-        if not layers_only:
+        if not annotation_only:
             ax.imshow(self.scan, cmap="gray")
+        if drusen:
+            visible = np.zeros(self.drusen.shape)
+            visible[self.drusen] = 1.0
+            ax.imshow(self.drusen, alpha=visible, cmap="Reds")
         for layer in layers:
             color = layers_color[layer]
             try: