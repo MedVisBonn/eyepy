5c51b4656d0dcc28d4b7c2ff0e8112e176746e28
Olivier Morelle <oli4morelle@gmail.com>
2021-08-16 10:30:27 +0200

    fix(eyepy/io/heyex): allow unknown heyex xml versions
    show a warning and use the base XML specification for HEYEX XML exports

--- eyepy/io/heyex/specification/xml_export/__init__.py
+++ eyepy/io/heyex/specification/xml_export/__init__.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
+import warnings
 from .v61240 import bscan_spec as v61240_bscan
 from .v61240 import oct_spec as v61240_oct
 from .v69530 import bscan_spec as v69530_bscan
 from .v69530 import oct_spec as v69530_oct
 
+from .base import bscan_base_spec, oct_base_spec
+
 
 def HEXML_VERSIONS(version):
-    versions = {"6.12.4.0": v61240_oct, "6.9.53.0": v69530_oct}
-    return versions[version]()
+    try:
+        versions = {"6.12.4.0": v61240_oct, "6.9.53.0": v69530_oct}
+        return versions[version]()
+    except KeyError:
+        warnings.warn("The XML export version is not known.")
+        return [(key, *value) for key, value in oct_base_spec().items()]
 
 
 def HEXML_BSCAN_VERSIONS(version):
-    versions = {"6.12.4.0": v61240_bscan, "6.9.53.0": v69530_bscan}
-    return versions[version]()
+    try:
+        versions = {"6.12.4.0": v61240_bscan, "6.9.53.0": v69530_bscan}
+        return versions[version]()
+    except KeyError:
+        warnings.warn("The XML export version is not known.")
+        return [(key, *value) for key, value in bscan_base_spec().items()]
