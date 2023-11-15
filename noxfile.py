b2bcf7bd85814b08affbd5964fed26f43d0b03eb
Olivier Morelle <oli4morelle@gmail.com>
2023-11-15 13:43:43 +0100

    build(eyepy): removes python 3.8 from testing matrix

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox  # type: ignore - nox is not a project dependency. This module is used by the global nox command, not this project
 
 
-@nox.session(python=['3.8', '3.9', '3.10', '3.11'])
+@nox.session(python=['3.9', '3.10', '3.11'])
 def tests(session):
     # Install the project dependencies
     session.install('poetry')
