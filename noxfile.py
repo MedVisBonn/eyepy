3c70173ddaafdb6399b9687224a688c274d3430e
Olivier Morelle <oli4morelle@gmail.com>
2026-05-29 14:45:30 +0200

    chore(ci): update test matrix and pre-commit tooling

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,17 @@
-import nox  # type: ignore - nox is not a project dependency. This module is used by the global nox command, not this project
+import nox
+
+nox.options.default_venv_backend = 'uv'
 
 
-@nox.session(
-    python=['3.9', '3.10', '3.11', '3.12', '3.13'],
-    venv_backend='uv',
-)
-def tests(session: nox.Session):
-    session.run_install(
-        'uv',
-        'sync',
-        '--all-groups',
-        f'--python={session.virtualenv.location}',
-        env={'UV_PROJECT_ENVIRONMENT': session.virtualenv.location},
-    )
-    session.run('pytest', *session.posargs)
+@nox.session(python=['3.10', '3.11', '3.12', '3.13'])
+@nox.parametrize('extras', [None, 'all'])
+def tests(session, extras):
+    """Run tests with and without optional dependencies."""
+    session.install('pytest', 'pytest-cov')
+
+    if extras == 'all':
+        session.install('.[all]')
+    else:
+        session.install('.')
+
+    session.run('pytest')
